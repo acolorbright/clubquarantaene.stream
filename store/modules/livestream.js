c462005940a74baab1fd646024ecd95abf73fb84
Florian Zia <florianzia@flozia.fritz.box>
2020-04-18 12:28:40 +0200

    feat: Add livestream state attributes

--- store/modules/livestream.js
+++ store/modules/livestream.js
@@ -1,4 +1,17 @@
-const state = () => ({});
+const state = () => ({
+  aspectRatio: '169',
+  delay: 0,
+  is360: true,
+  isMoving: false,
+  isMuted: false,
+  isRunning: false,
+  isSupported: true,
+  // Latency setting: https://support.google.com/youtube/answer/7444635?hl=en&ref_topic=9257892
+  latency: 'normal-latency', // low-latency, ultra-low-latency
+  numberOfViewers: 0,
+  orientation: {},
+  resolution: 1440
+});
 
 const mutations = {};
 

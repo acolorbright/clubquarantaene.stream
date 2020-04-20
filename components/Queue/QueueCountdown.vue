bb3b897eb03155198d99ed1ed1b9222d43e472e5
Florian Zia <florianzia@flozia.fritz.box>
2020-04-20 18:42:15 +0200

    feat: Queue countdown

--- components/Queue/QueueCountdown.vue
+++ components/Queue/QueueCountdown.vue
@@ -9,8 +9,26 @@
 export default {
   data() {
     return {
-      countdown: 30
+      countdown: 10,
+      countdownInterval: null
     };
+  },
+  beforeDestroy() {
+    clearInterval(this.countdownInterval);
+  },
+  mounted() {
+    this.handleCountdown();
+  },
+  methods: {
+    handleCountdown() {
+      this.countdownInterval = setInterval(() => {
+        if (this.countdown > 0) {
+          this.countdown -= 1;
+        } else {
+          clearInterval(this.countdownInterval);
+        }
+      }, 1000);
+    }
   }
 };
 </script>

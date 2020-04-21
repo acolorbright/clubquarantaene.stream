cfda9067a0801ca0d3ddc5b3af52d01c81c75dc6
Florian Zia <florianzia@flozia.fritz.box>
2020-04-21 16:31:55 +0200

    feat: Add test color picker

--- components/Queue/views/Decision/Enjoy.vue
+++ components/Queue/views/Decision/Enjoy.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
-    <h3 class="step-title">Okay, enjoy and pick your color.</h3>
-    <input type="text" />
+    <h3 class="step-title">
+      Okay, enjoy and pick your color.
+    </h3>
+
+    <slider-picker v-model="colors" class="step-color" />
 
     <div class="step-buttons-btn">
       <button class="step-buttons" @click="enterClub">Enter club</button>
@@ -10,7 +13,38 @@
 </template>
 
 <script>
+import { Slider } from 'vue-color';
+
 export default {
+  components: {
+    'slider-picker': Slider
+  },
+  data() {
+    return {
+      colors: {
+        hex: '#194d33',
+        hsl: {
+          h: 150,
+          s: 0.5,
+          l: 0.2,
+          a: 1
+        },
+        hsv: {
+          h: 150,
+          s: 0.66,
+          v: 0.3,
+          a: 1
+        },
+        rgba: {
+          r: 25,
+          g: 77,
+          b: 51,
+          a: 1
+        },
+        a: 1
+      }
+    };
+  },
   methods: {
     enterClub() {
       alert('enter club');

cb4a4e77ce4ec97cacf4a8fe680a6eccea084b6a
Marco Land <marco.anmeldung@gmail.com>
2020-04-23 00:28:47 +0200

    cleanup: content store

--- store/modules/content.js
+++ store/modules/content.js
@@ -201,42 +201,6 @@ const state = () => ({
   questionsAnswered: false
 });
 
-const mutations = {
-  setContent(state, data) {
-    state.data = data;
-    state.data.event.start = moment.tz(state.data.event.start, 'Europe/Berlin');
-    state.data.event.end = moment.tz(state.data.event.end, 'Europe/Berlin');
-    state.eventIsRunning = moment()
-      .tz('Europe/Berlin')
-      .isBetween(state.data.event.start, state.data.event.end);
-    state.eventIsOver = moment()
-      .tz('Europe/Berlin')
-      .isAfter(state.data.event.end);
-  },
-  setEventIsRunning(state, data) {
-    state.eventIsRunning = data;
-    state.eventIsOver = moment()
-      .tz('Europe/Berlin')
-      .isAfter(state.data.event.end);
-  },
-  nextQuestion(state) {
-    state.currentQuestion++;
-    if (state.currentQuestion === state.questions.length) {
-      state.questionsAnswered = true;
-    }
-  }
-};
-
-// const actions = {
-//   checkIfEventIsRunning (state, commit) {
-//     const eventIsRunning = moment().tz('Europe/Berlin').isBetween(state.data.event.start, state.data.event.end)
-//     if (eventIsRunning !== state.eventIsRunning) {
-//       commit('setEventIsRunning', eventIsRunning)
-//     }
-//   }
-// }
-
 export default {
-  state,
-  mutations
+  state
 };

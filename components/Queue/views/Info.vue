b52aeada5625606ae2be190fbdc677c3765e290a
Florian Zia <florianzia@flozia.fritz.box>
2020-04-22 02:22:20 +0200

    feat: Stop countdown on info steps

--- components/Queue/views/Info.vue
+++ components/Queue/views/Info.vue
@@ -3,15 +3,19 @@
     <h3 class="step-title">{{ data.title }} ({{ data.type }})</h3>
     <p class="step-text">{{ data.text }}</p>
 
-    <div class="step-buttons">
-      <button class="step-buttons-btn" @click="nextStep">
-        understood
-      </button>
-    </div>
+    <transition name="fade-step" mode="out-in">
+      <div v-if="!countdownIsRunning" class="step-buttons">
+        <button class="step-buttons-btn" @click="nextStep">
+          understood
+        </button>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   props: {
     data: {
@@ -19,9 +23,29 @@ export default {
       default: null
     }
   },
+  computed: {
+    countdownIsRunning() {
+      return this.$store.state.queue.countdown.isRunning;
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.pauseCountdown();
+    }, 2000);
+  },
   methods: {
+    ...mapActions({
+      setCountdownIsRunning: 'setCountdownIsRunning'
+    }),
     nextStep() {
       this.$emit('nextStep', true);
+      this.resumeCountdown();
+    },
+    pauseCountdown() {
+      this.setCountdownIsRunning(false);
+    },
+    resumeCountdown() {
+      this.setCountdownIsRunning(true);
     }
   }
 };

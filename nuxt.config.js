e3b320cccd9ea7dc31c4df94bc9b66f791c2cea2
Marco Land <marco.anmeldung@gmail.com>
2020-04-25 21:04:10 +0200

    change: times

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,8 @@ export default {
     CMS_URL: process.env.CMS_URL,
     SOCKET_URL: process.env.SOCKET_URL,
     startEventDate: '04-24-2020 21:59 PM',
-    endEventDate: '04-26-2020 22:32 PM',
-    closedEventDate: '04-26-2020 23:59 AM',
+    endEventDate: '04-26-2020 10:32 AM',
+    closedEventDate: '04-26-2020 23:59 PM',
     YOUTUBE_VIDEO_ID: 'bZeZ9KD4RvQ',
     YOUTUBE_VIDEO_ID_ENDED: 'grhFTe071VA'
   },

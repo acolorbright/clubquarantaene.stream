0390ae778cc808509f786e5f1a5896770cd66cec
Florian Zia <florianzia@flozia.fritz.box>
2020-06-25 09:50:02 +0200

    chore: Remove personal invitation query

--- middleware/checkPermission.js
+++ middleware/checkPermission.js
@@ -11,18 +11,6 @@ export default ({ store, redirect, route }) => {
   const isQueue = route.name === 'index';
   const shouldRedirectToQueue = !accessGranted && !isQueue;
 
-  const { personalinvitationfrom } = route.query;
-  const isInvited =
-    personalinvitationfrom && personalinvitationfrom === 'clubquarantaene';
-
-  if (isInvited && isQueue && !accessGranted) {
-    setTimeout(() => {
-      alert(
-        'Hey friend of clubquarantäne, thank you for following our invitation. Have fun! <3'
-      );
-    }, 3000);
-  }
-
   if (shouldRedirectToQueue && !isDebugMode && isProtectedRoute) {
     return redirect('/');
   }

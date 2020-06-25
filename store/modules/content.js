c4c38411696229fe63952370650d4886bc3b11ff
Marco Land <marcoland@Marcos-MBP.fritz.box>
2020-06-25 17:24:07 +0200

    update: timetable

--- store/modules/content.js
+++ store/modules/content.js
@@ -34,170 +34,170 @@ const state = () => ({
     {
       name: 'I$A',
       start: '22:00',
-      end: '23:00',
+      end: '23:30',
       day: '2020-06-26',
-      img: 'CQ_ISA.jpg'
+      img: 'cq_isa.jpg'
     },
     {
       name: 'Stenny',
       start: '23:30',
       end: '01:00',
       day: '2020-06-27',
-      img: 'CQ_Stenny.jpg'
+      img: 'cq_stenny.jpg'
     },
     {
       name: 'Lucy',
       start: '01:00',
       end: '02:30',
       day: '2020-06-27',
-      img: 'CQ_Lucy.jpg'
+      img: 'cq_lucy.jpg'
     },
     {
       name: 'KI/KI ',
       start: '02:30',
       end: '04:00',
       day: '2020-06-27',
-      img: 'CQ_KI_KI.jpg'
+      img: 'cq_ki_ki.jpg'
     },
     {
       name: 'Somewhen ',
       start: '04:00',
       end: '05:30',
       day: '2020-06-27',
-      img: 'CQ_Somewhen.jpg'
+      img: 'cq_somewhen.jpg'
     },
     {
       name: 'Peach',
       start: '05:30',
       end: '07:00',
       day: '2020-06-27',
-      img: 'CQ_Peach.jpg'
+      img: 'cq_peach.jpg'
     },
     {
       name: 'THC',
       start: '07:00',
       end: '08:30',
       day: '2020-06-27',
-      img: 'CQ_THC.jpg'
+      img: 'cq_thc.jpg'
     },
     {
       name: 'Analog Soul',
       start: '08:30',
       end: '10:00',
       day: '2020-06-27',
-      img: 'CQ_analogsoul.jpg'
+      img: 'cq_analogsoul.jpg'
     },
     {
       name: 'DEBONAIR',
       start: '10:00',
       end: '11:30',
       day: '2020-06-27',
-      img: 'CQ_DEBONAIR.jpg'
+      img: 'cq_debonair.jpg'
     },
     {
       name: 'Bergsonist ',
       start: '11:30',
       end: '13:00',
       day: '2020-06-27',
-      img: 'CQ_BergSonist.jpg'
+      img: 'cq_bergsonist.jpg'
     },
     {
       name: 'Upsammy B2B Oceanic',
       start: '13:00',
       end: '14:30',
       day: '2020-06-27',
-      img: 'CQ_Upsammy_b2b_Oceanic.jpg'
+      img: 'cq_upsammy_b2b_oceanic.jpg'
     },
     {
       name: 'Russell E.L. Butler',
       start: '14:30',
       end: '16:00',
       day: '2020-06-27',
-      img: 'CQ_Russel_EL_Butler.jpg'
+      img: 'cq_russel_el_butler.jpg'
     },
     {
       name: 'Hodge',
       start: '16:00',
       end: '17:30',
       day: '2020-06-27',
-      img: 'CQ_Hodge.jpg'
+      img: 'cq_hodge.jpg'
     },
     {
       name: 'Dixon',
       start: '17:30',
       end: '19:00',
       day: '2020-06-27',
-      img: 'CQ_Dixon.jpg'
+      img: 'cq_dixon.jpg'
     },
     {
       name: 'rRoxymore',
       start: '19:30',
-      end: '21:00',
+      end: '20:30',
       day: '2020-06-27',
-      img: 'CQ_rRoxymore.jpg'
+      img: 'cq_rroxymore.jpg'
     },
     {
       name: 'Nazira',
-      start: '21:00',
-      end: '22:30',
+      start: '20:30',
+      end: '22:00',
       day: '2020-06-27',
-      img: 'CQ_Nazira.jpg'
+      img: 'cq_nazira.jpg'
     },
     {
       name: 'Freddy K',
       start: '22:30',
-      end: '00:00',
-      day: '2020-06-28',
-      img: 'CQ_Freddy_K.jpg'
+      end: '23:30',
+      day: '2020-06-27',
+      img: 'cq_freddy_k.jpg'
     },
     {
       name: 'Rifts',
-      start: '00:00',
-      end: '01:30',
+      start: '23:30',
+      end: '01:00',
       day: '2020-06-28',
-      img: 'CQ_Rifts.jpg'
+      img: 'cq_rifts.jpg'
     },
     {
       name: 'Rødhåd',
-      start: '01:30',
-      end: '03:00',
+      start: '01:00',
+      end: '02:30',
       day: '2020-06-28',
-      img: 'CQ_Rodhad.jpg'
+      img: 'cq_rodhad.jpg'
     },
     {
       name: 'Héctor Oaks',
-      start: '03:00',
-      end: '04:30',
+      start: '02:30',
+      end: '04:00',
       day: '2020-06-28',
-      img: 'CQ_Hector_Oaks.jpg'
+      img: 'cq_hector_oaks.jpg'
     },
     {
       name: 'Aurora Halal',
-      start: '04:30',
-      end: '06:00',
+      start: '04:00',
+      end: '05:30',
       day: '2020-06-28',
-      img: 'CQ_Aurora_Halal.jpg'
+      img: 'cq_aurora_halal.jpg'
     },
     {
       name: 'Paál',
-      start: '06:00',
-      end: '07:30',
+      start: '05:30',
+      end: '07:00',
       day: '2020-06-28',
-      img: 'CQ_Paal.jpg'
+      img: 'cq_paal.jpg'
     },
     {
       name: 'IMOGEN',
-      start: '07:30',
-      end: '09:00',
+      start: '07:00',
+      end: '08:30',
       day: '2020-06-28',
-      img: 'CQ_IMOGEN.jpg'
+      img: 'cq_imogen.jpg'
     },
     {
       name: 'Palms Trax',
-      start: '09:00',
-      end: '11:30',
+      start: '08:30',
+      end: '11:00',
       day: '2020-06-28',
-      img: 'CQ_Palms_Trax.jpg'
+      img: 'cq_palms_trax.jpg'
     }
   ],
   bathroomImages: [
